2e01414db3ad4a75b6c5e0a8cf87b32105315e54
nethraa99 <nethraasai87@gmail.com>
2025-10-17 00:55:00 +0530

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/mini_project2");
+const db = mongoose.connection;
+ 
+db.on('connected', (err) => {
+    if(!err){
+        console.log("Database connected successfully");
+    }
+})
+
+module.exports = db;
